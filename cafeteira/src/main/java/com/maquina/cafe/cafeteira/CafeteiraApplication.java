999d7534acb1fb39218ce5b1a502f8dc51374430
marcelo oliverio <marceloliverio.aae@gmail.com>
2023-06-22 09:52:03 -0300

    Criação do projeto em dupla - cafeteira

---
+++ cafeteira/src/main/java/com/maquina/cafe/cafeteira/CafeteiraApplication.java
@@ -0,0 +1,13 @@
+package com.maquina.cafe.cafeteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteiraApplication.class, args);
+	}
+
+}
---
+++ cafeteira/src/test/java/com/maquina/cafe/cafeteira/CafeteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maquina.cafe.cafeteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
